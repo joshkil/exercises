ce1a5edf534c1d335da75c8211f4a58d6fad8d41
joshkil <joshkil@SEA-1800017326.ant.amazon.com>
2016-09-25 18:14:00 -0700

    Fixing Classpath

--- CodingExercises/test/com/searattled/exercises/LongestPallendromeTest.java
+++ CodingExercises/test/com/searattled/exercises/LongestPallendromeTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
-public class TestLongestPallendrome {
+public class LongestPallendromeTest {
 
 	@Test
 	public void testFindLongestPallendrome() {
